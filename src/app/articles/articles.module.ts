b3198d2301f7384dcfdfcbd2066039216ed99a38
Karl Herrick <me@karlherrick.com>
2019-03-08 20:28:02 -0500

    import esm version of x-postpress

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -1,9 +1,9 @@
+import 'x-postpress/build/esm-bundled/src/components/x-postpress';
 import { ArticlesComponent } from './articles.component';
 import { ArticlesRoutingModule } from './articles-routing.module';
 import { CommonModule } from '@angular/common';
-import { isPlatformBrowser } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgModule } from '@angular/core';
-import { PLATFORM_ID, CUSTOM_ELEMENTS_SCHEMA, Inject } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -16,12 +16,4 @@ import { PLATFORM_ID, CUSTOM_ELEMENTS_SCHEMA, Inject } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
-export class ArticlesModule {
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {
-    if (isPlatformBrowser(platformId)) {
-      require('x-postpress/build/es5-bundled/src/components/x-postpress')
-    }
-  }
-}
+export class ArticlesModule {}
